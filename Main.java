753bad0b8ff66c3dbcfb928df2cb577b13fd8e60
Lucas Laukka <45783656+LucasLaukka@users.noreply.github.com>
2018-12-16 17:41:18 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package HuvudKod;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String[] names = new String[7];
+		
+		names[0] = "Lucas";
+		names[1] = "Hanna";
+		names[2] = "Mattias";
+		names[3] = "Noa";
+		names[4] = "Clara";
+		names[5] = "Amitis";
+		names[6] = "Jocke";
+	
+		
+		
+		for (int i = 0 ; i < names.length ; i++) {
+			System.out.println( (i + 1) + ". " + names[i]);
+			System.out.println();
+		}
+		
+		
+		
+
+	}
+
+}
